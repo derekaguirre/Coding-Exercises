677c22698a7e3aa56b4e2bb1ed9f0bf3d65a72ae
derekaguirre <derekaguirre@me.com>
2023-05-17 02:54:22 -0600

    Solved 20. Valid Parentheses

---
+++ Java/Progress/ValidParenthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+//Submission: https://leetcode.com/problems/valid-parentheses/submissions/951959449/
+
+public class ValidParenthesis {
+    public static void main(String[] args) {
+        String input = "asda()afgasd";
+        System.out.println(isValid(input));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char iter : s.toCharArray()) {
+            if (iter == '(' || iter == '{' || iter == '[')
+                stack.push(iter);
+            else if (iter == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            else if (iter == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            else if (iter == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
+            else
+                return false;
+        }
+        return stack.empty();
+    }
+}
