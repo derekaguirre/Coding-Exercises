5a048298997394b5d5f3dc5a40428797517ff602
derekaguirre <derekaguirre@me.com>
2023-06-15 21:28:30 -0600

    Solved 104. Maximum Depth of Binary Tree with BFS

--- Java/Completed/MaxDepthBinaryTree/MaxDepthBinaryTree.java
+++ Java/Completed/MaxDepthBinaryTree/MaxDepthBinaryTree.java
@@ -1,32 +1,43 @@
 package Completed.MaxDepthBinaryTree;
 
-public class MaxDepthBinaryTree{
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-    public static void main(String[] args) {
-    }
+public class MaxDepthBinaryTree {
 
-
-    //BFS
-    //LevelOrder
-
-    //DFS
-    //Preorder  Rlr
-    //Inorder   lRr
-    //Postorder lrR
-
-    //Recursive Depth First Search
+    // Recursive Depth First Search
     public int maxDepth(TreeNode root) {
-        if(root == null)
+        if (root == null)
             return 0;
-        
+
         return Math.max(maxDepth(root.left) + 1, (maxDepth(root.right) + 1));
     }
 
-    //Iterative DFS
-
-
-
-    //Iterative BFS
+    // Iterative DFS Preorder
     
-    
+
+    // Iterative BFS (Level Order)
+    public int maxDepthBFS(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            level++;
+        }
+        return level;
+    }
+
 }
