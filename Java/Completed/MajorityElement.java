fe63dc0befaa6ba809e675355781b296fa9b9eff
derekaguirre <derekaguirre@me.com>
2023-06-16 00:55:49 -0600

    Solved 88. Merge Sorted Array

--- Java/Completed/MajorityElement.java
+++ Java/Completed/MajorityElement.java
@@ -1,4 +1,4 @@
-package Progress;
+package Completed;
 
 import java.util.Map;
 import java.util.HashMap;
--- Java/Completed/MergeSortedArray.java
+++ Java/Completed/MergeSortedArray.java
@@ -1,23 +1,42 @@
 package Completed;
+// Submission: https://leetcode.com/problems/merge-sorted-array/submissions/972411632/
 
-//Submission: https://leetcode.com/submissions/detail/892514513/
+class MergeSortedArray {
 
-public class MergeSortedArray {
-    // nums1.length == m + n
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 3, 0, 0, 0 };
+        int[] arr2 = { 2, 5, 6 };
+        int m = 3;
+        int n = 3;
 
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+        // int[] arr1 = {1};
+        // int[] arr2 = {};
+        // int m = 1;
+        // int n = 0;
+
+        // int[] arr1 = {0};
+        // int[] arr2 = {1};
+        // int m = 1;
+        // int n = 0;
+
+        merge(arr1, m, arr2, n);
+
+    }
+
+    public static void merge(int[] arr1, int m, int[] arr2, int n) {
+        int l1Len = m - 1; // length of the nonzero first list
+        int l2Len = n - 1; // length of the second list
+        int mergeLen = m + n - 1; // length of the list
+
+        while (l2Len >= 0) {
+            // If arr2 contains bigger element then merge it and move positions
+            if (l1Len >= 0 && arr1[l1Len] > arr2[l2Len])
+                arr1[mergeLen--] = arr1[l1Len--];
+            // If arr1 has a bigger or same element as arr2 then move it up in the list
+            else {
+                arr1[mergeLen--] = arr2[l2Len--];
             }
-        }
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+
         }
     }
 }
