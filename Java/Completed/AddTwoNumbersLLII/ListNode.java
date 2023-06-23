9931b567b14c09352ea0c12e6dd39801c074b16f
derekaguirre <derekaguirre@me.com>
2023-06-22 21:34:24 -0600

    Solved 203. Remove Linked List Elements

---
+++ Java/Completed/AddTwoNumbersLLII/ListNode.java
@@ -0,0 +1,18 @@
+package Progress.addTwoNumsLL;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
