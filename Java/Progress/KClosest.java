78c0e7a2434773cf313578333e60adc95dc33de8
derekaguirre <derekaguirre@me.com>
2023-06-26 15:59:45 -0600

    Solved 94. Binary Tree Inorder Traversal

--- Java/Progress/KClosest.java
+++ Java/Progress/KClosest.java
@@ -1,5 +1,9 @@
 package Progress;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 //Problem: https://leetcode.com/problems/k-closest-points-to-origin/description/
 
 public class KClosest {
@@ -7,75 +11,51 @@ public class KClosest {
         // int[][] points = { { 1, 3 }, { -2, 2 } };
         int[][] points = { { 3, 3 }, { 5, -1 }, { -2, 4 } };
         // int[][] points = { { 0, 1 }, { 1, 0 } };
-
         kClosest(points, 2);
 
+        
+    }
+    
+    public static int[][] kClosestMax(int[][] points, int k) {
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> (b[0] * b[0] + b[1] * b[1] - (a[0] * a[0] + a[1] * a[1])));
+        
+        for (int[] point : points) {
+            maxHeap.add(point);
+            
+            if (maxHeap.size() > k) {
+                maxHeap.poll();
+            }
+        }
+        
+        int[][] result = new int[k][2];
+        while (k-- > 0) {
+            result[k] = maxHeap.poll();
+        }
+        
+        return result;
     }
 
-    public static int[][] kClosest(int[][] points, int k) {
-        int[][] result = new int[k][2];
-        int resultRow = 0;
+    public static int[][] kClosestSort(int[][] points, int k) {
+        Arrays.sort(points, (p1, p2) -> euclideanDistance(p1) - euclideanDistance(p2));
+        return Arrays.copyOfRange(points,0,k);
+    }
+    
+    public static int[][] kClosestPrioQ(int[][] points, int k){
+        final PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(
+            Comparator.comparing(this::euclideanDistance)
+        );
 
-        double min = Integer.MAX_VALUE;
-        for (int iter = 0; iter < result.length; iter++) {
-            // Set value of min with the first evaluation in the points list
-            // Retrieve the x and y points of every data point
-            double currentMin = Integer.MAX_VALUE;
-            for (int rows = 0; rows < result[iter].length; rows++) {
-                
-                // Evaluate both sides of equation
-                double side1 = Math.pow(points[rows][0] - 0, 2);
-                double side2 = Math.pow(points[rows][1] - 0, 2);
-                double distance = Math.sqrt(side1 + side2);
-
-                // && currentMin < min
-                // When smallest is found, save it to result
-                System.out.println("Min: " + min);
-                System.out.println("currentMin: " + currentMin);
-                System.out.println("distance: "+ distance);
-                System.out.println("is 'distance:"+distance+ "' <= 'min: " + min+"'? " + (distance <= min && distance < currentMin));
-                System.out.println();
-                if(currentMin > min && currentMin != Integer.MAX_VALUE){
-                    System.out.println("found a currentMin bigger than min");
-                }
-                if (distance <= min && distance < currentMin) {
-                    // This means the current minimum is distance
-                    currentMin = distance;
-                    System.out.println("Current Min: " + currentMin);
-                    
-                    // Check if the distance is the second largest
-                    // If distance is >= to min
-                    // If the first point in the result does not match the current point
-                    // If the second point in the result does not match the current point
-                    if (distance == min && result[rows][0] != points[rows][0] && result[rows][1] != points[rows][1]) {
-                        System.out.println("same distance at different points found, updating the result");
-                        resultRow++;
-                        result[resultRow][0] = points[rows][0];
-                        result[resultRow][1] = points[rows][1];
-                        iter++;
-                        System.out.println("result[" + resultRow + "][0] = " + points[rows][0] + "\n" +
-                    "result[" + resultRow + "][1] = " + points[rows][1]);
-                        break;
-                    }
-                    
-                    // Update the result
-                    System.out.println("unique min found, updating the result");
-                    result[resultRow][0] = points[rows][0];
-                    result[resultRow][1] = points[rows][1];
-                    
-                    System.out.println("result[" + resultRow + "][0] = " + points[rows][0] + "\n" +
-                    "result[" + resultRow + "][1] = " + points[rows][1]);
-                    // min = distance;
-                }
-                
-                //update the current min 
-            }
-            System.out.println("Inner loop done, updating the min");
-            min = Math.min(min, currentMin);
-            
-            resultRow++;
+        for(int[] point: points){
+            priorityQueue.offer(point);
         }
 
+        final int[][] result = new int[k][2];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = priorityQueue.poll();
+        }
         return result;
+    }
+    private static int euclideanDistance(int[] point){
+        return point[0] * point[0] + point[1] + point[1];
     }
 }
---
+++ Java/Progress/LongestPalindrome.java
@@ -0,0 +1,38 @@
+package Progress;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+        // System.out.println(longestPalindrome("aacabdkacaa"));
+    }
+
+    public static String longestPalindrome(String s) {
+        // If string is short
+        if (s.length() == 1)
+            return s;
+        if (s.length() == 2 && s.charAt(0) != s.charAt(1)) {
+            return String.valueOf(s.charAt(0));
+        }
+        // Sliding window approach
+        // babad = len5
+        char[] charArr = s.toCharArray();
+        String tmpStr = "";
+        int front = 0;
+        while (front < charArr.length - 1) {
+            int back = charArr.length - 1;
+            while (back > 0) {
+                // If they match,
+                if (charArr[front] == charArr[back] && charArr[front + 1] == charArr[back - 1]) {
+                    tmpStr = s.substring(front, back + 1);
+                    front = charArr.length;
+                    break;
+                }
+                back--;
+            }
+            front++;
+        }
+
+        return tmpStr;
+    }
+
+
+}
