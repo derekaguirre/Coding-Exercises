074509e07a2a51f408cd8c18f20bd73b2e5f21da
derekaguirre <derekaguirre@me.com>
2023-07-04 20:28:56 -0600

    Solved 242. Valid Anagram

---
+++ Java/Completed/ValidAnagram.java
@@ -0,0 +1,32 @@
+package Completed;
+
+import java.util.Arrays;
+
+//Submission: https://leetcode.com/problems/valid-anagram/submissions/986575630/
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+
+        String s = "car";
+        String t = "rat";
+
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        char[] sCharArr = s.toCharArray();
+        char[] tCharArr = t.toCharArray();
+
+        Arrays.sort(sCharArr);
+        Arrays.sort(tCharArr);
+
+        for (int i = 0; i < tCharArr.length; i++) {
+            if (sCharArr[i] != tCharArr[i])
+                return false;
+        }
+        return true;
+    }
+}
