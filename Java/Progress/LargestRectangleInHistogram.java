dbbda6e5563a664b0202c41edf04d263b9b9b0b6
derekaguirre <derekaguirre@me.com>
2023-07-21 21:58:31 -0600

    Adjusted 84. Largest Rectangle in Histogram

--- Java/Progress/LargestRectangleInHistogram.java
+++ Java/Progress/LargestRectangleInHistogram.java
@@ -1,5 +1,7 @@
 package Progress;
 
+//Problem: https://leetcode.com/problems/largest-rectangle-in-histogram/
+
 public class LargestRectangleInHistogram {
     public static void main(String[] args) {
         int[] heights = {2,4};
@@ -13,13 +15,14 @@ public class LargestRectangleInHistogram {
     // return max
     public static int largestRectangleArea(int[] heights) {
         if(heights.length == 0)
-            return 0;
+        return 0;
         if(heights.length == 1)
-            return heights[0];
+        return heights[0];
         int pointer2 = 1;
         int product = 0;
         int max = 0;
         for (int i = 0; pointer2 < heights.length; i++, pointer2++) {
+            int diff = Math.abs(heights[i] - heights[pointer2]);
             // Equalize the numbers and get product
             if (heights[i] < heights[pointer2])
                 product = heights[i] * 2;
