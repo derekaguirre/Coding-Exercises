cd3bd1748770d7fef2e1a0d65b0a7125cd3ce1f4
derekaguirre <derekaguirre@me.com>
2023-07-24 17:58:32 -0600

    Solved 125. Valid Palindrome

--- Java/Completed/ValidPalindrome.java
+++ Java/Completed/ValidPalindrome.java
@@ -1,13 +1,17 @@
 package Completed;
 
-//Submission:  https://leetcode.com/problems/valid-palindrome/submissions/970760069/
+//Submission:  https://leetcode.com/problems/valid-palindrome/submissions/1003076543/
 
 public class ValidPalindrome {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome2("A man, a plan, a canal: Panama"));
+    }
+
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int i = 0;
         int j = s.length() - 1;
-        while(i <= j) {
+        while (i <= j) {
             if (s.charAt(i) != s.charAt(j)) {
                 return false;
             }
@@ -17,5 +21,17 @@ public class ValidPalindrome {
         return true;
     }
 
-    //2nd Implementation
+    // 2nd Implementation
+    public static boolean isPalindrome2(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int front = 0;
+        int back = s.length()-1;
+        while (front <= back) {
+            if(s.charAt(front) != s.charAt(back))
+                return false;
+            front++;
+            back--;
+        }
+        return true;
+    }
 }
