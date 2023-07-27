9a76a0894620e1e3ca5160efcd62ca1f1238e718
derekaguirre <derekaguirre@me.com>
2023-07-27 09:34:44 -0600

    Solved 704. Binary Search

--- Java/Completed/BinarySearch.java
+++ Java/Completed/BinarySearch.java
@@ -4,16 +4,15 @@ package Completed;
 
 public class BinarySearch {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(target == nums[mid])
+        int low = 0, high = nums.length - 1, mid = 0;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if (target == nums[mid])
                 return mid;
-            else if(target > nums[mid]) 
-                low = mid+1;
+            else if (target > nums[mid])
+                low = mid + 1;
             else
-                high = mid-1;
+                high = mid - 1;
         }
         return -1;
     }
