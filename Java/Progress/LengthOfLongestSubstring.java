eed90bdc6d710deac44937bfce804b2208258870
derekaguirre <derekaguirre@me.com>
2023-07-31 19:04:49 -0600

    Solved 3. Longest Substring Without Repeating Characters

--- Java/Progress/LengthOfLongestSubstring.java
+++ Java/Progress/LengthOfLongestSubstring.java
@@ -1,41 +1,30 @@
 package Progress;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
-//Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+//Submission: https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/1008946760/
 
 public class LengthOfLongestSubstring {
-    public int lengthOfLongestSubstring(String s) {
-        Map<Character, Integer> map = new HashMap<>(); 
-        char[] charArr = s.toCharArray();
-
-        for (int i = 0; i < charArr.length; i++) {
-            if(map.containsKey(s.charAt(i))){
-
-            }
-        }
-
-        return 0;
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
-    
-    public int lengthOfLongestSubstring2(String s) {
-        int i = 0, j = 0, max = 0;
+    public static int lengthOfLongestSubstring(String s) {
+        int slow = 0, fast = 0, highestSubstr = 0;
         Set<Character> set = new HashSet<>();
-        
-        while (j < s.length()) {
-            if (!set.contains(s.charAt(j))) {
-                set.add(s.charAt(j++));
-                max = Math.max(max, set.size());
-            } else {
-                set.remove(s.charAt(i++));
-            }
+        // Move the fast pointer through the string
+        while (fast < s.length()) {
+            // If unique add to set and keep track of max value of substring
+            if (!set.contains(s.charAt(fast))) {
+                set.add(s.charAt(fast++));
+                highestSubstr = Math.max(highestSubstr, set.size());
+                // Otherwise if a value is found remove it from the set
+                // and move pointer to check for the next duplicate value
+            } else
+                set.remove(s.charAt(slow++));
         }
-        
-        return max;
+        return highestSubstr;
     }
 }
-
