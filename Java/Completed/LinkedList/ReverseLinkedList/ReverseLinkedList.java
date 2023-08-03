4ff2d8f4301dd3341e1744a73abdaa4538ced2a6
derekaguirre <derekaguirre@me.com>
2023-08-02 21:27:33 -0600

    Improved 206. Reverse Linked List

--- Java/Completed/LinkedList/ReverseLinkedList/ReverseLinkedList.java
+++ Java/Completed/LinkedList/ReverseLinkedList/ReverseLinkedList.java
@@ -3,26 +3,47 @@ package Completed.LinkedList.ReverseLinkedList;
 import java.util.Stack;
 
 //Submission: https://leetcode.com/submissions/detail/924059958/
+//Submission2: https://leetcode.com/problems/reverse-linked-list/submissions/1010800906/
 
 public class ReverseLinkedList {
-    public ListNode reverseList(ListNode head) {
-        if(head == null){
+
+    //Iterative O(n) time, O(n) space
+    public ListNode reverseListStack(ListNode head) {
+        if (head == null)
             return head;
-        }
         Stack<Integer> stack = new Stack<Integer>();
-        
-        while(head != null){
+
+        while (head != null) {
             stack.push(head.val);
             head = head.next;
         }
         ListNode reverseHead = new ListNode(stack.pop());
         ListNode currentNode = reverseHead;
-        while(stack.size() != 0){
+        while (stack.size() != 0) {
             currentNode.next = new ListNode(stack.pop());
             currentNode = currentNode.next;
         }
-            
-        
+
         return reverseHead;
+    }
+    
+    //Iterative O(n) time, O(1) space
+    public ListNode reverseListIterPointers(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode prev = null, curr = head;
+        //Set pointer to prev node
+        while(curr != null){
+            //Save next for moving forward
+            ListNode tmpNext = curr.next;
+
+            //Set the pointer to prev node
+            curr.next = prev;
+
+            //Move nodes forward
+            prev = curr;
+            curr = tmpNext;
+        }
+        return prev;
     }
 }
