aaae7dad6322991c21e3c6a05dcabb71d27a0708
derekaguirre <derekaguirre@me.com>
2023-08-05 22:36:00 +0530

    Added 424. Longest Repeating Character Replacement

---
+++ Java/Completed/TwoPointers/SlidingWindow/LongestRepeatingCharReplacement.java
@@ -0,0 +1,12 @@
+package Completed.TwoPointers.SlidingWindow;
+
+public class LongestRepeatingCharReplacement {
+    public int characterReplacement(String s, int k) {
+        //Two pointers:
+        //  slow, fast which is ahead by one
+        //slow keeps track of the front of the substring
+        //fast moves if the next char matches the first pointer's char and increment the length
+        //otherwise, if another char exists, substract k if above 0 and add to length
+        return 0;
+    }
+}
