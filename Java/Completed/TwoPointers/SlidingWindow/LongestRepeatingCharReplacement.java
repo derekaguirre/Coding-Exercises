add34e97a384546d7e551a13e705eac8f124cb2e
derekaguirre <derekaguirre@me.com>
2023-08-06 19:48:38 -0600

    Solved 424. Longest Repeating Character Replacement

--- Java/Completed/TwoPointers/SlidingWindow/LongestRepeatingCharReplacement.java
+++ Java/Completed/TwoPointers/SlidingWindow/LongestRepeatingCharReplacement.java
@@ -1,12 +1,38 @@
 package Completed.TwoPointers.SlidingWindow;
 
+import java.util.HashMap;
+import java.util.Map;
+//Submission: https://leetcode.com/problems/longest-repeating-character-replacement/submissions/1014275173/
+
 public class LongestRepeatingCharReplacement {
     public int characterReplacement(String s, int k) {
-        //Two pointers:
-        //  slow, fast which is ahead by one
-        //slow keeps track of the front of the substring
-        //fast moves if the next char matches the first pointer's char and increment the length
-        //otherwise, if another char exists, substract k if above 0 and add to length
-        return 0;
+        // holds all repeated characters
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        int left = 0, maxRepeatCount = 0, maxWindow = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            // increase current char count
+            char ch = s.charAt(right);
+            // put character count in map
+            charCountMap.put(ch, charCountMap.getOrDefault(ch, 0) + 1);
+
+            // max repeated char count
+            maxRepeatCount = Math.max(maxRepeatCount, charCountMap.get(ch));
+
+            int currentSizeOfWindow = right - left + 1;
+            // if current size of the window - max repeated count greater than k (number of edits)
+            if (currentSizeOfWindow - maxRepeatCount > k) {
+                char charToDecrease = s.charAt(left);
+                // decrease char count 
+                charCountMap.put(charToDecrease, charCountMap.get(charToDecrease) - 1);
+                left++;
+
+                // update current size of window
+                currentSizeOfWindow = right - left + 1;
+            }
+
+            maxWindow = Math.max(maxWindow, currentSizeOfWindow);
+        }
+        return maxWindow;
     }
 }
