04b68fe1212351e61b62f6e16eb0d0f9f8d24df1
derekaguirre <derekaguirre@me.com>
2023-08-12 22:00:10 -0600

    Added 989. Add to Array-Form of Integer

---
+++ Java/Progress/AddToArrayForm.java
@@ -0,0 +1,21 @@
+package Progress;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Problem: https://leetcode.com/problems/add-to-array-form-of-integer/description/
+
+public class AddToArrayForm {
+    public static void main(String[] args) {
+        addToArrayForm(new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, 1);
+    }
+
+    public static List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = num.length - 1; i > 0; i--) {
+
+        }
+
+        return list;
+    }
+}
---
+++ Java/Progress/MatrixBlockSum.java
@@ -0,0 +1,19 @@
+package Progress;
+
+public class MatrixBlockSum {
+    public int[][] matrixBlockSum(int[][] mat, int k) {
+        int[][] tmp = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        //0 - 1 <= r <= 0 + 1
+        //0 - 1 <= c <= 0 + 1
+        /*
+        [12,21,16],
+        [27,45,33],
+        [24,39,28]
+        */
+        return new int[][] {{}};
+    }
+}
