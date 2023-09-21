e40f0120568ab30004beb3499d242cfda08fd876
derekaguirre <derekaguirre@me.com>
2023-09-21 05:29:06 -0600

    Added submission for partion

--- Java/Progress/FindTheValueOfThePartition.java
+++ Java/Progress/FindTheValueOfThePartition.java
@@ -14,22 +14,12 @@ public class FindTheValueOfThePartition {
     }
 
     public static int findTheValueOfThePartition(int[] nums) {
-        // |max(nums1) - min(nums2)|
-        // 1,2,3,4
-        // 1,2 3,4
-        // 2 - 3 = 1
-        // |i - i + 1 |
-        // 1,2,3,3,4 should result in zero
-
         Arrays.sort(nums);
         int val = Integer.MAX_VALUE;
         //Prevent out of bounds
         for (int i = 0; i <= nums.length - 2; i++) {
             // Partition the two numbers
             int eval =  Math.abs(nums[i] - nums[i + 1]);
-            //Shouldn't be needed
-            if (eval == 0)
-                return 0;
             //Keep track of the min value of the evaluations
             val = Math.min(eval, val);
         }
