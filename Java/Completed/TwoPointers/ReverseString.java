e255e037f37390276967e750aea1511ae71729e7
derekaguirre <derekaguirre@me.com>
2023-10-16 22:34:50 -0600

    Solved 344. Reverse String

--- Java/Completed/TwoPointers/ReverseString.java
+++ Java/Completed/TwoPointers/ReverseString.java
@@ -1,9 +1,19 @@
 package Completed.TwoPointers;
 
 //Submission: https://leetcode.com/submissions/detail/776461170/
+//Submission2: https://leetcode.com/problems/reverse-string/submissions/1076364810/
 
 public class ReverseString {
-    public void reverseString(char[] s) {
+
+    public static void main(String[] args) {
+        char[] s = { 'o', 'l', 'l', 'e' };
+        reverseString(s);
+        for (int i = 0; i < s.length; i++) {
+            System.out.println("FINAL EVAL" + s[i]);
+        }
+    }
+
+    public static void reverseString(char[] s) {
         int left = 0, right = s.length - 1;
         while (left < right) {
             char temp = s[left];
@@ -11,6 +21,19 @@ public class ReverseString {
             s[right] = temp;
             left++;
             right--;
+        }
+    }
+
+    // O(n) time
+    // O(1) space
+    public static void reverseString2(char[] s) {
+        int front = 0, back = s.length - 1;
+        while (front <= back) {
+            char tmpFront = s[front];
+            s[front] = s[back];
+            s[back] = tmpFront;
+            front++;
+            back--;
         }
     }
 }
