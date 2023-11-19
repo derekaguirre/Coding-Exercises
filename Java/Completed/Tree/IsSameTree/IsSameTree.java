792c8d9daac2270ea58456828ba358a29dca4878
derekaguirre <derekaguirre@me.com>
2023-11-19 22:36:00 +0530

    Updated 100. Same Tree

--- Java/Completed/Tree/IsSameTree/IsSameTree.java
+++ Java/Completed/Tree/IsSameTree/IsSameTree.java
@@ -4,13 +4,14 @@ package Completed.Tree.IsSameTree;
 
 public class IsSameTree {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) 
+        if (p == null && q == null)
             return true;
-        
-        if (p == null || q == null) 
+
+        if (p == null || q == null)
             return false;
-        
-        if (p.val != q.val) return false;
+
+        if (p.val != q.val)
+            return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-      }
+    }
 }
