e06018b29bd31891de17fab3293cf2fad47ebbd6
derekaguirre <derekaguirre@me.com>
2023-11-26 18:54:25 -0700

    Solved 572. Subtree of Another Tree

---
+++ Java/Completed/Tree/IsSubTree/IsSubTree.java
@@ -0,0 +1,23 @@
+package Completed.Tree.IsSubTree;
+
+//Submission: https://leetcode.com/problems/subtree-of-another-tree/submissions/1107121066/
+
+public class IsSubTree {
+    public boolean isSubtree(TreeNode s, TreeNode t) { // takes O(m x n)
+        if (s == null) {
+            return t == null;
+        }
+        return isSame(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSame(TreeNode t1, TreeNode t2) { // takes O(n)
+        if (t1 == null && t2 == null)
+            return true;
+        if (t1 == null || t2 == null)
+            return false;
+
+        if (t1.val != t2.val)
+            return false;
+        return isSame(t1.left, t2.left) && isSame(t1.right, t2.right);
+    }
+}
