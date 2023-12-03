45f30be40033407633952f9501cb79f8dc245481
derekaguirre <derekaguirre@me.com>
2023-12-03 22:36:00 +0530

    Revisited 141. Linked List Cycle

--- Java/Completed/LinkedList/LinkedListCycleI/LinkedListCycleI.java
+++ Java/Completed/LinkedList/LinkedListCycleI/LinkedListCycleI.java
@@ -2,6 +2,7 @@ package Completed.LinkedList.LinkedListCycleI;
 
 //Submission: https://leetcode.com/problems/linked-list-cycle/submissions/956862999/
 //Submission 2: https://leetcode.com/problems/linked-list-cycle/submissions/1073121503/
+//Submission 3: https://leetcode.com/problems/linked-list-cycle/submissions/1171148432
 
 public class LinkedListCycleI {
     public boolean hasCycle(ListNode head) {
@@ -17,6 +18,24 @@ public class LinkedListCycleI {
             slow = slow.next;
             fast = fast.next.next;
 
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean hasCycle2(ListNode head) {
+        //Check if the list is empty
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode fast = head, slow = head;
+
+        // Iterate one twice as fast until both pointers match
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
             if (fast == slow)
                 return true;
         }
