4b3813614776835a80d274f0ba7d1faa11f1b8f5
derekaguirre <derekaguirre@me.com>
2023-12-05 22:36:00 +0530

    Adjusted 9. Palindrome Number

--- Java/Completed/TwoPointers/IsPalindrome.java
+++ Java/Completed/TwoPointers/IsPalindrome.java
@@ -1,7 +1,6 @@
 package Completed.TwoPointers;
 
 //Submission: https://leetcode.com/problems/palindrome-number/submissions/964136631/
-//Submission 2: https://leetcode.com/problems/valid-palindrome/submissions/1175908667
 
 public class IsPalindrome {
     public static boolean isPalindrome(int x) {
@@ -14,18 +13,6 @@ public class IsPalindrome {
                 return false;
             pointer1++;
             pointer2--;
-        }
-        return true;
-    }
-
-    public static boolean isPalindrome2(String s) {
-        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        int front = 0;
-        int back = s.length() - 1;
-        while (front <= back) {
-            // If they're not equal then its not a palindrome
-            if (s.charAt(front++) != s.charAt(back--))
-                return false;
         }
         return true;
     }
