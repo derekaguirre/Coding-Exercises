5e7662c08e0260d873aab924c8f72dbd83508461
derekaguirre <derekaguirre@me.com>
2025-04-23 14:08:07 -0600

    Revisited problems - 217. Contains Duplicate

--- Java/Completed/HashTable/ContainsDuplicate.java
+++ Java/Completed/HashTable/ContainsDuplicate.java
@@ -13,7 +13,7 @@ public class ContainsDuplicate {
         }
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
-            if(nums[i] == nums[i-1]){
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
         }
@@ -22,15 +22,29 @@ public class ContainsDuplicate {
 
     public static boolean containsDuplicateAttempt2(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
-        
+
         for (int i = 0; i < nums.length; i++) {
-            if(!map.containsKey(nums[i]))
+            if (!map.containsKey(nums[i]))
                 map.put(nums[i], 1);
             else
-                map.put(nums[i], map.get(nums[i])+1);
+                map.put(nums[i], map.get(nums[i]) + 1);
 
-            if(map.get(nums[i]) > 1)
+            if (map.get(nums[i]) > 1)
                 return true;
+        }
+        return false;
+    }
+
+    public boolean hasDuplicateAttempt3(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        if (nums.length == 0 || nums.length == 1)
+            return false;
+        for (int iter = 0; iter < nums.length; iter++) {
+            // if found in the map then end, it's a duplicate
+            if (map.containsKey(nums[iter]))
+                return true;
+            // otherwise keep adding to the map
+            map.put(nums[iter], 1);
         }
         return false;
     }
